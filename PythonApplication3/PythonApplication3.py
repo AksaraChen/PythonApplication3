6f31412ea25fe9294dc9af1b3be0cc788f95e378
則叡 陳 <chenray603@gmail.com>
2019-03-21 06:39:19 +0800

    calculator

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,3 +1,32 @@
-letters=['a','b','c']
-for l in letters:
-    print(l)
+while True:
+    print("enter 'add' to add two numbers")
+    print("enter 'substract to substract two numbers'")
+    print("enter 'multiply' to multiply two numbers")
+    print("enter 'divide' to divide two numbers")
+    print("enter 'quit' to end the program")
+    i=input(":")
+
+    if i=="quit":
+        break;
+    elif i=="add":
+        a=float(input("Enter a number"))
+        b=float(input("Enter another number"))
+        answer=str(a+b)
+        print("The answer is"+ answer)
+    elif i=="substract":
+        a=float(input("Enter a number"))
+        b=float(input("Enter another number"))
+        answer=str(a-b)
+        print("The answer is"+ answer)
+    elif i=="multiply":
+        a=float(input("Enter a number"))
+        b=float(input("Enter another number"))
+        answer=str(a*b)
+        print("The answer is"+ answer)
+    elif i=="divide":
+        a=float(input("Enter a number"))
+        b=float(input("Enter another number"))
+        answer=str(a/b)
+        print("The answer is"+ answer)
+    else:
+        print("Error:wrong input")
